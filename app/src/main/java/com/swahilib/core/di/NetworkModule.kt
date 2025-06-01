bf68690d44ee9dbe59a6ec807ee51caaf13707be
Siro Daves <sirodevske@gmail.com>
2025-06-01 08:33:45 +0300

    supabase configurations into di

--- app/src/main/java/com/swahilib/core/di/NetworkModule.kt
+++ app/src/main/java/com/swahilib/core/di/NetworkModule.kt
@@ -1,26 +1,24 @@
 package com.swahilib.core.di
 
 import com.swahilib.BuildConfig
-import com.swahilib.core.helpers.Supabase
+import com.swahilib.core.helpers.MySupabase
 import com.swahilib.core.utils.ApiConstants
 import com.swahilib.data.sources.remote.ApiService
 import dagger.*
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import io.github.jan.supabase.*
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import javax.inject.Named
+import javax.inject.*
 
 @InstallIn(SingletonComponent::class)
 @Module
-@Suppress("unused")
 object NetworkModule {
-
     @Provides
     @Reusable
-    @JvmStatic
     fun provideApiService(@Named("swahilibApi") retrofit: Retrofit): ApiService {
         return retrofit.create(ApiService::class.java)
     }
@@ -28,29 +26,32 @@ object NetworkModule {
     @Provides
     @Named("swahilibApi")
     @Reusable
-    @JvmStatic
-    fun provideSwahilibApi(
-        okHttpClient: OkHttpClient.Builder,
-        supabase: Supabase,
-    ): Retrofit {
+    fun provideSwahilibApi(okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
             .baseUrl(ApiConstants.BASE)
             .addConverterFactory(GsonConverterFactory.create())
-            .client(okHttpClient.build())
+            .client(okHttpClient)
             .build()
     }
 
     @Provides
     @Reusable
-    @JvmStatic
-    internal fun provideOkHttp(): OkHttpClient.Builder {
-        val logging = HttpLoggingInterceptor()
-        logging.level = HttpLoggingInterceptor.Level.BODY
-
-        val okHttpClient = OkHttpClient.Builder()
-        if (BuildConfig.DEBUG) {
-            okHttpClient.addInterceptor(logging)
+    fun provideOkHttp(): OkHttpClient {
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
         }
-        return okHttpClient
+
+        return OkHttpClient.Builder()
+            .apply {
+                if (BuildConfig.DEBUG) {
+                    addInterceptor(logging)
+                }
+            }
+            .build()
     }
+
+    @Provides
+    @Singleton
+    fun provideSupabaseClient(): SupabaseClient = MySupabase.client
+
 }
