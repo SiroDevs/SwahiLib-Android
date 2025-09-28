bddcd4ab038fd01aeff6f93018dc9fc6ad4d7272
Siro Daves <sirodevske@gmail.com>
2025-09-28 23:33:45 +0300

    revenuecat intergrations

--- app/src/main/java/com/swahilib/presentation/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/swahilib/presentation/screens/splash/SplashScreen.kt
@@ -1,18 +1,8 @@
 package com.swahilib.presentation.screens.splash
 
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.*
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.*
 import androidx.navigation.NavHostController
-import com.swahilib.R
 import com.swahilib.domain.repository.PrefsRepository
 import com.swahilib.presentation.navigation.Routes
 import kotlinx.coroutines.delay
@@ -36,71 +26,4 @@ fun SplashScreen(navController: NavHostController) {
     }
 
     SplashContent()
-}
-
-@Preview(showBackground = true)
-@Composable
-fun SplashContent() {
-    Scaffold(
-        content = {
-            Box(
-                modifier = Modifier
-                    .padding(it)
-                    .fillMaxSize()
-                    .background(color = MaterialTheme.colorScheme.onPrimary)
-            ) {
-                Column(
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    Spacer(Modifier.weight(1f))
-                    Image(
-                        painter = painterResource(id = R.drawable.app_icon),
-                        contentDescription = "App logo",
-                        modifier = Modifier.size(200.dp)
-                    )
-                    Spacer(Modifier.height(10.dp))
-                    Text(
-                        text = "SwahiLib",
-                        style = TextStyle(
-                            fontSize = 50.sp,
-                            letterSpacing = 5.sp,
-                            fontWeight = FontWeight.Bold,
-                            color = MaterialTheme.colorScheme.primary
-                        )
-                    )
-                    Spacer(Modifier.weight(1f))
-                    Divider(
-                        color = MaterialTheme.colorScheme.onPrimaryContainer,
-                        thickness = 2.dp,
-                        modifier = Modifier
-                            .padding(horizontal = 10.dp)
-                            .height(1.dp)
-                    )
-                    KiswahiliKitukuzwe()
-                    Spacer(Modifier.height(50.dp))
-                }
-            }
-        },
-    )
-}
-
-
-@Composable
-fun KiswahiliKitukuzwe() {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center
-    ) {
-        Text(
-            text = "Kiswahili Kitukuzwe",
-            style = TextStyle(
-                fontSize = 25.sp,
-                letterSpacing = 5.sp,
-                fontWeight = FontWeight.Bold,
-                color = MaterialTheme.colorScheme.primary
-            )
-        )
-    }
 }
