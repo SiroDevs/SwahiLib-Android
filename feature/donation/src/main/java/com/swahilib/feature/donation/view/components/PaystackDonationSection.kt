fd2776ef9cc93d814049b7c921b893f5bfb5ed3d
Siro Devs <sirodevske@gmail.com>
2026-08-22 20:33:45 +0300

    minor fixes

--- feature/donation/src/main/java/com/swahilib/feature/donation/view/components/PaystackDonationSection.kt
+++ feature/donation/src/main/java/com/swahilib/feature/donation/view/components/PaystackDonationSection.kt
@@ -4,14 +4,10 @@ import android.util.Patterns
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.SnackbarHostState
